00ca67b49014943b1d297e93836750743571b0db
Try-Yang <601238706@qq.com>
2017-10-29 20:39:48 +0800

    后台首页完成

---
+++ public/back/js/index.js
@@ -0,0 +1,64 @@
+$(function () {
+  var myChart = echarts.init(document.querySelector(".pic_left"));
+  var option = {
+    title: {
+      text: '2017年注册人数'
+    },
+    tooltip: {},
+    legend: {
+      data:['人数']
+    },
+    xAxis: {
+      data: ["1月","2月","3月","4月","5月","6月"]
+    },
+    yAxis: {},
+    series: [{
+      name: '人数',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }]
+  };
+  
+  myChart.setOption(option);
+  
+  var myChart2 = echarts.init(document.querySelector(".pic_right"))
+  option2 = {
+    title : {
+      text: '热门品牌',
+      subtext: '纯属虚构',
+      x:'center'
+    },
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['耐克','阿迪','李宁','新百伦','安踏']
+    },
+    series : [
+      {
+        name: '销量情况',
+        type: 'pie',
+        radius : '55%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'耐克'},
+          {value:310, name:'阿迪'},
+          {value:234, name:'李宁'},
+          {value:135, name:'新百伦'},
+          {value:1548, name:'安踏'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+  myChart2.setOption(option2);
+})
