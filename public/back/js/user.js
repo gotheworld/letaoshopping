51c86eab8b2695ef63b778ab919d1ea9a90041b0
Try-Yang <601238706@qq.com>
2017-10-29 21:50:46 +0800

    分页功能完成

---
+++ public/back/js/user.js
@@ -0,0 +1,29 @@
+$(function () {
+  
+  var pagenum = 1;
+  var pagesize = 5;
+  function fn(){
+    $.ajax({
+      type: 'get',
+      url: '/user/queryUser',
+      data: {page: pagenum, pageSize: pagesize},
+      success: function (data) {
+        console.log(data);
+        var html = template("tpl", data);
+        $('tbody').html(html);
+        $(".pagination").bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: pagenum,
+          totalPages: Math.ceil(data.total/pagesize),
+          size: "small",
+          onPageClicked: function (event, originalEvent, type, page) {
+            pagenum = page;
+            fn()
+          }
+        });
+      }
+    })
+  }
+  fn()
+ 
+})
